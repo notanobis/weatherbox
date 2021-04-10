8448b8ba5b0efb01ee2907733d4810e8570b68a2
giotaChita <69098448+giotaChita@users.noreply.github.com>
2021-04-10 13:26:57 +0100

    Update humidity.py

--- humidity.py
+++ humidity.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT
 import os
 import time
-#from humidity import take_data
+#import humidity.py as  
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 
@@ -9,16 +9,9 @@ DHT_PIN = 4
 
 def take_data():
     humidity, temperature =  Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    
-    return humidity, temperature
+    d = {'date': time.strftime('%m/%d/%y'),
+         'time' : time.strftime('%H:%M'),
+         'tem':temperature,
+         'hum' : humidity}
+    return d    
 
-'''while True:
-    humidity, temperature =  Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-    if humidity is not None and temperature is not None:
-        print('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'),time.strftime('%H:%M'),temperature, humidity))
-
-    else:
-        print("Failed to retrieve data from humidity sensor")
-    time.sleep(1)
-'''
